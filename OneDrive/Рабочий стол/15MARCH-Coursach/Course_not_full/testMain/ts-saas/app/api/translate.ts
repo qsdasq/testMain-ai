0239472e09d9b00b86317ac8e338a32181f733cc
qsdasq <155198328+qsdasq@users.noreply.github.com>
2024-03-15 10:32:13 +0500

    Delete OneDrive/Рабочий стол/15MARCH-Coursach/Course_not_full/testMain/ts-saas directory

--- OneDrive/Рабочий стол/15MARCH-Coursach/Course_not_full/testMain/ts-saas/app/api/translate.ts
+++
@@ -1,6 +0,0 @@
-import {translate} from '@vitalets/google-translate-api';
-
-export async function translateText(text: string): Promise<string> {
-    const result = await translate(text, {to: 'en'});
-    return result.text;
-}
